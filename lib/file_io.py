c58b3f0f3bd4149b1f45a636c561c378a7fc3713
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-06 08:17:16 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as txt_file:
+        txt_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as txt_file:
+        txt_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", 'r') as txt_file:
+        for line in txt_file:
+            return line
